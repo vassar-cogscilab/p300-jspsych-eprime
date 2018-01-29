447b3eb85fcb8447655fcab0ae2f6f3b36a9ab89
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-01-29 14:27:00 -0500

    update analysis docs

--- analysis/anonymize-data.R
+++ analysis/anonymize-data.R
@@ -1,7 +1,9 @@
+set.seed(12604)
 library(readr)
 library(dplyr)
 library(hashmap)
-all.data = read_csv("data/eeg/generated/eeg-subset.csv")
+library(httr)
+
 code.table <- expand.grid(sw=c('js', 'ep'), el=1:128)
 code.table$combined.code <- NA
 for(i in 1:nrow(code.table)){
@@ -10,6 +12,8 @@ for(i in 1:nrow(code.table)){
 code.table$code <- sample(nrow(code.table))
 code.lookup <- hashmap(code.table$combined.code, code.table$code)
 
+# this grabs the eeg-subset.csv file from the OSF, since it is too big to commit to the GH repository
+all.data <- httr::GET("https://osf.io/v4sfk/?action=download", progress()) %>% content(as='parsed')
 coded.data <- all.data %>% rowwise() %>% mutate(code = code.lookup[[paste0(software,electrode)]])
 
 coded.data$software <- NULL
@@ -17,3 +21,8 @@ coded.data$electrode <- NULL
 
 write_csv(code.table, "data/eeg/generated/eeg-code-table.csv")
 write_csv(coded.data, "data/eeg/generated/eeg-coded.csv")
+
+
+timestamp()
+data <- read.csv(url('https://osf.io/v4sfk/?action=download'))
+timestamp()
--- analysis/load-data-from-raw.R
+++ analysis/load-data-from-raw.R
@@ -1,7 +1,8 @@
-ibrary(tidyr)
+library(tidyr)
 library(dplyr)
+library(readr)
 
-subjects <- c(1:9,11:15)
+subjects <- c(1:9,11:27,29:30)
 conditions <- c('Visual Common', 'Visual Rare', 'Audio Common', 'Audio Rare')
 software <- c('ep', 'js')
 electrodes.to.include <- c(19, 11, 4, 12, 5, 118, 111, 104, 93, 86, 78, 62, 61, 53, 42, 36, 29, 20, 6, 112, 105, 87, 79, 54, 37, 30, 13, 7, 106, 80, 55, 31, 60, 67, 72, 77, 85, 66, 71, 76, 84, 75)
@@ -19,20 +20,21 @@ extract.data <- function(file, subject, condition, software){
   return(data)
 }
 all.data <- NA
+pb <- txtProgressBar(max=max(subjects), style=3)
 for(s in subjects){
   for(condition in conditions){
     for(sw in software){
       rx <- paste0('.*-0*',s,'-.*',condition,'.*',sw,'.*')
       file <- list.files(path='data/eeg', pattern=rx)
       data <- extract.data(file[1], s, condition, sw)
-      if(is.na(all.data)){
+      if(all(is.na(all.data[1]))){
         all.data <- data
       } else {
         all.data <- rbind(all.data, data)
       }
     }
   }
+  setTxtProgressBar(pb, s)
 }
 
-write.csv(all.data, file="data/eeg/generated/eeg-subset.csv")
-
+write_csv(all.data, file="data/eeg/generated/eeg-subset.csv", row.names = FALSE)
